ed517bb81c2682ad091c83e24459f9f1b743d2a1
erdnusskeks <erdnusskeks@googlemail.com>
2015-01-30 11:26:49 +0100

    Improve configurations

--- config.rb
+++ config.rb
@@ -62,6 +62,9 @@ configure :build do
       { icon: "apple-touch-icon-152x152-precomposed.png" },
       { icon: "apple-touch-icon-114x114-precomposed.png" },
       { icon: "apple-touch-icon-72x72-precomposed.png" },
+      { icon: "favicon.png", size: "16x16" },                     # The classic favicon, displayed in the tabs.
+      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },   # Used by IE, and also by some other browsers if we are not careful.
+      { icon: "favicon-32x32.png" },                              # For Safari on Mac OS.
     ]
   }
 
