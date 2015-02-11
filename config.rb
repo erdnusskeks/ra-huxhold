19df6abb94a3ea81f5bbd94078efa01876126569
erdnusskeks <erdnusskeks@googlemail.com>
2015-02-11 11:17:20 +0100

    Delete the favicons

--- config.rb
+++ config.rb
@@ -59,16 +59,7 @@ set :images_dir, 'img'
 
 # Build-specific configuration
 configure :build do
-  activate :favicon_maker, :icons => {
-    "_favicon_template.png" => [
-      { icon: "apple-touch-icon-152x152-precomposed.png" },
-      { icon: "apple-touch-icon-114x114-precomposed.png" },
-      { icon: "apple-touch-icon-72x72-precomposed.png" },
-      { icon: "favicon.png", size: "16x16" },                     # The classic favicon, displayed in the tabs.
-      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },   # Used by IE, and also by some other browsers if we are not careful.
-      { icon: "favicon-32x32.png" },                              # For Safari on Mac OS.
-    ]
-  }
+  activate :favicon_maker
 
   # For example, change the Compass output style for deployment
   # activate :minify_css
